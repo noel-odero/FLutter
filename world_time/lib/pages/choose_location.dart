8574b3979ec2478a7bac9c06ff9532088662492c
Noel Odero <anoel.odero@gmail.com>
2025-09-24 06:32:01 +0200

    maps and routing

--- world_time/lib/pages/choose_location.dart
+++ world_time/lib/pages/choose_location.dart
@@ -10,6 +10,15 @@ class ChooseLocation extends StatefulWidget {
 class _ChooseLocationState extends State<ChooseLocation> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(body: Text('Choose Location'));
+    return Scaffold(
+      backgroundColor: Colors.grey[200],
+      appBar: AppBar(
+        backgroundColor: Colors.blue[900],
+        title: Text('Choose a location'),
+        centerTitle: true,
+        elevation: 0,
+      ),
+      body: Text('Choose a location'),
+    );
   }
 }
