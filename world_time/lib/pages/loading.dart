d0dee28d4c1717e8a68c4a63e767f2b391a6d5cb
Noel Odero <anoel.odero@gmail.com>
2025-09-24 19:26:55 +0200

    custom classes

--- world_time/lib/pages/loading.dart
+++ world_time/lib/pages/loading.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:http/http.dart';
 import 'dart:convert';
+import 'package:world_time/services/world_time.dart';
 
 class Loading extends StatefulWidget {
   const Loading({super.key});
@@ -10,32 +11,31 @@ class Loading extends StatefulWidget {
 }
 
 class _LoadingState extends State<Loading> {
-  void getTime() async {
-    // make the reequest
-    Response response = await get(
-      Uri.parse('https://worldtimeapi.org/api/timezone/Europe/London'),
+  String time = 'loading';
+
+  void setupWorldTime() async {
+    WorldTime instance = WorldTime(
+      flag: 'german.png',
+      location: 'Berlin',
+      url: 'Europe/Berlin',
     );
-    Map data = jsonDecode(response.body);
-    // print(data);
-
-    String datetime = data['datetime'];
-    String offset = data['utc_offset'].substring(1, 3);
-    // print(datetime);
-    // print(offset);
-
-    DateTime now = DateTime.parse(datetime);
-    now = now.add(Duration(hours: int.parse(offset)));
-    print(now);
+    await instance.getTime();
+    print(instance.time);
+    setState(() {
+      time = instance.time;
+    });
   }
 
   @override
   void initState() {
     super.initState();
-    getTime();
+    setupWorldTime();
   }
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(body: Text('loading screen...'));
+    return Scaffold(
+      body: Padding(padding: EdgeInsets.all(50.0), child: Text(time)),
+    );
   }
 }
---
+++ world_time/lib/services/world_time.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/material.dart';
+import 'package:http/http.dart';
+import 'dart:convert';
+
+class WorldTime {
+  String location;
+  String time = '';
+  String flag;
+  String url;
+
+  WorldTime({required this.flag, required this.location, required this.url});
+
+  Future<void> getTime() async {
+    // make the reequest
+    Response response = await get(
+      Uri.parse('https://worldtimeapi.org/api/timezone/$url'),
+    );
+    Map data = jsonDecode(response.body);
+    // print(data);
+
+    String datetime = data['datetime'];
+    String offset = data['utc_offset'].substring(1, 3);
+    // print(datetime);
+    // print(offset);
+
+    DateTime now = DateTime.parse(datetime);
+    now = now.add(Duration(hours: int.parse(offset)));
+
+    time = now.toString();
+  }
+}
